4504f24755f29f72c4f29bcf3b7219311922c50f
Mohammad Naim Dahee <dahee.naim@gmail.com>
2022-02-19 22:53:34 +0100

    Student Filters Added

--- src/app/components/student/add-student/add-student.component.ts
+++ src/app/components/student/add-student/add-student.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Student } from 'src/app/models/Student';
 
 @Component({
   selector: 'app-add-student',
   templateUrl: './add-student.component.html',
-  styleUrls: ['./add-student.component.css']
+  styleUrls: ['./add-student.component.css'],
 })
 export class AddStudentComponent implements OnInit {
+  student: Student = {
+    firstName: '',
+    lastName: '',
+    photoUrl: '',
+    email: '',
+    isActive: true,
+  };
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  onSubmit = (form: NgForm) => {};
 }
